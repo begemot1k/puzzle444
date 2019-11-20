5666169f4e57e587a72219a6e45eb6dc3219a6cf
Vadim Kh <khamnuev.v.aleks@sberbank.ru>
2019-11-20 16:32:44 +0300

    Initial Commit

---
+++ puzzle444/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  puzzle444
+//
+//  Created by 01587913 on 20.11.2019.
+//  Copyright © 2019 vadim khamnuev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
